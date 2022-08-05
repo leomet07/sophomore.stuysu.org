b813690feba31c1dac5747631868c5e45ef4d3ae
leomet07 <leomet07@gmail.com>
2022-08-05 13:58:43 -0400

    Add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,23 @@
 import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import logoImg from "../public/img/logo.png";
+import Image from "next/image";
 
 const Navbar = () => {
 	return (
 		<nav id={styles.nav}>
+			<Link href="/" passHref>
+				<Image
+					src={logoImg}
+					id={styles.logo}
+					alt="Sophomore Caucus and the SU"
+					width="50"
+					height="50"
+				/>
+			</Link>
 			<div id={styles.links_container}>
 				<div className={styles.link}>
-					<Link href="/contact">Talk to Us</Link>
+					<Link href="/contact">Contact</Link>
 				</div>
 				<div className={styles.link}>
 					<Link href="/policies">Policies</Link>
