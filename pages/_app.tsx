290a21cd4b77580fbd4e96480ff3ce199891b629
leomet07 <leomet07@gmail.com>
2022-08-05 14:03:53 -0400

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Script
 				async
 				defer
