ef3c4f5f68d17ea4d189efc849e9bec1df02e061
leomet07 <leomet07@gmail.com>
2022-08-05 12:33:38 -0400

    Add umami tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Script
+				async
+				defer
+				data-website-id="1b6c4e62-b876-4452-97fb-658d7bc3a98b"
+				src="https://umami-fork-alpha.vercel.app/umami.js"
+			></Script>
 			<div id="navbar_parent">
 				<Navbar />
 			</div>
