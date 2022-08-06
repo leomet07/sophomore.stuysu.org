6fb3d5556132f7ceb227d1ba21c11062605e324b
David Chen <dchen07@protonmail.com>
2022-08-06 19:49:25 -0400

    Add preliminary work on limiter div
    This is the start of proper control for the decorative background circle

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,20 +22,25 @@ const Contact: NextPage = () => {
 
                 <div className={styles.prompt}>
                     <p>Let’s Talk!</p>
-                    <div className={styles.circle}/>
-                </div>
-                <div className={styles.buttons}>
-                <IconContext.Provider value={{color: 'white', className: styles.button}}>
-                    <a href={'https://www.instagram.com/sophomorecaucus/'} target={'_blank'} rel="noreferrer">
-                        <SiInstagram />
-                    </a>
-                    <a href={'https://www.facebook.com/StuySophCaucus'} target={'_blank'} rel="noreferrer">
-                        <SiFacebook />
-                    </a>
-                    <a href={'https://github.com/leomet07/sophomore.stuysu.org'} target={'_blank'} rel="noreferrer">
-                        <SiGithub />
-                    </a>
-                </IconContext.Provider>
+                    <div className={styles.limiter}>
+                        <div className={styles.circle}/>
+                        <div className={styles.buttons}>
+                            <IconContext.Provider value={{color: 'white', className: styles.button}}>
+                                <a href={'https://www.instagram.com/sophomorecaucus/'} target={'_blank'} rel="noreferrer">
+                                    <SiInstagram />
+                                </a>
+                                <a href={'https://www.facebook.com/StuySophCaucus'} target={'_blank'} rel="noreferrer">
+                                    <SiFacebook />
+                                </a>
+                                <a href={'https://github.com/leomet07/sophomore.stuysu.org'} target={'_blank'} rel="noreferrer">
+                                    <SiGithub />
+                                </a>
+                            </IconContext.Provider>
+                        </div>
+                        <p id={styles.feedback}>
+                            Got feedback for the caucus? Provide it anonymously <a href={'https://forms.gle/uMz4TydXi1h1biHf8'} target={'_blank'} rel="noreferrer">here</a>.
+                        </p>
+                    </div>
                 </div>
 
             </main>
