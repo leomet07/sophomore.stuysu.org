5d4d6e1098e78d285b6c533717442d269ed5e982
David Chen <dchen07@protonmail.com>
2022-08-06 18:56:58 -0400

    Add icons with links to Contact
    Imports the react-icons npm package for iconography

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Contact.module.css";
+import { IconContext } from "react-icons";
+import { SiInstagram, SiFacebook, SiGithub } from "react-icons/si";
+
 
 const Contact: NextPage = () => {
     return (
@@ -20,6 +23,19 @@ const Contact: NextPage = () => {
                 <div className={styles.prompt}>
                     <p>Let’s Talk!</p>
                     <div className={styles.circle}/>
+                </div>
+                <div className={styles.buttons}>
+                <IconContext.Provider value={{color: 'white', className: styles.button}}>
+                    <a href={'https://www.instagram.com/sophomorecaucus/'} target={'_blank'} rel="noreferrer">
+                        <SiInstagram />
+                    </a>
+                    <a href={'https://www.facebook.com/StuySophCaucus'} target={'_blank'} rel="noreferrer">
+                        <SiFacebook />
+                    </a>
+                    <a href={'https://github.com/leomet07/sophomore.stuysu.org'} target={'_blank'} rel="noreferrer">
+                        <SiGithub />
+                    </a>
+                </IconContext.Provider>
                 </div>
 
             </main>
