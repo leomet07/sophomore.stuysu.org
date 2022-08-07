c4868866b6cbf86fc9c41cffda72eecc33632fdb
leomet07 <leomet07@gmail.com>
2022-08-06 22:22:16 -0400

    Changed around the pill buttons on the schedule widget

--- components/ScheduleWidget.tsx
+++ components/ScheduleWidget.tsx
@@ -3,8 +3,11 @@ import styles from "../styles/ScheduleWidget.module.css";
 const ScheduleWidget = () => {
 	return (
 		<div id={styles.container}>
-			<div id={styles.schedule_type}>
+			<div className={styles.pill} id={styles.schedule_type}>
 				<span>Regular</span>
+			</div>
+			<div className={styles.pill} id={styles.current_time}>
+				<span>12:23 PM</span>
 			</div>
 			<div id={styles.schedule_widget}>
 				<div className={styles.bottom}>
@@ -14,7 +17,7 @@ const ScheduleWidget = () => {
 						&nbsp;passed
 					</p>
 				</div>
-				<h1 id={styles.current_time}>12:23 PM</h1>
+				<h1 id={styles.current_period}>Period 8</h1>
 				<div className={styles.bottom}>
 					<p>
 						<span className={styles.highlight}>36</span> min
