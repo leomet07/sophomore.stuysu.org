a60b0c24531e0a7fce4a67147ff7d6ce8091271a
leomet07 <leomet07@gmail.com>
2022-08-07 20:44:51 -0400

    prepend https:// to the production URL
    next requires ABSOLUTE urls, not just the domain

--- lib/getServerUrl.ts
+++ lib/getServerUrl.ts
@@ -1,7 +1,9 @@
 function getServerUrl() {
 	const dev = process.env.NODE_ENV !== "production";
 
-	return dev ? "http://localhost:3000" : process.env.NEXT_PUBLIC_VERCEL_URL;
+	return dev
+		? "http://localhost:3000"
+		: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL;
 }
 
 export default getServerUrl;
