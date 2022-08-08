d160453b2d8cf6b40909490b446ee9447f5bce7a
leomet07 <leomet07@gmail.com>
2022-08-07 20:03:25 -0400

    Use vercel url for production site

--- lib/getServerUrl.ts
+++ lib/getServerUrl.ts
@@ -1,7 +1,7 @@
 function getServerUrl() {
 	const dev = process.env.NODE_ENV !== "production";
 
-	return dev ? "http://localhost:3000" : "https://sophomore.stuysu.org";
+	return dev ? "http://localhost:3000" : process.env.VERCEL_URL;
 }
 
 export default getServerUrl;
