c99861b41347cec3774225b93de5a9750bcce775
David Chen <dchen07@protonmail.com>
2022-08-08 19:36:01 -0400

    Add unadded edits
    There were some edits I did not properly `git add` in the previous
    commit.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Profile from "../components/Profile";
+import { Profile, Director } from "../components/Profile";
 import styles from "../styles/About.module.css";
 
 const About: NextPage = () => {
@@ -15,7 +15,7 @@ const About: NextPage = () => {
 			</Head>
 			<main className={styles.main}>
 				<h1 className={styles.title}>ABOUT US</h1>
-				<div className={styles.directors}>
+				<div className={styles.presidents}>
 					<Profile imgSrc={'/img/profiles/fin.jpg'} imgAlt={'_placehodler'} >
 						<p>
 							According to all known laws of aviation, there is no way a bee should be able to fly.
@@ -35,10 +35,14 @@ const About: NextPage = () => {
 						</p>
 					</Profile>
 				</div>
+				<div className={styles.directors}>
+					<Director imgSrc={'/img/profiles/david.jpg'} imgAlt={'theegghead27 pfp'} description={'david (egg head)'}/>
+					<Director imgSrc={'/img/profiles/lenny.jpg'} imgAlt={'leomet07 pfp'} description={'lenny (leomet)'}/>
+					<Director imgSrc={'/img/profiles/will.jpg'} imgAlt={'willpill pfp'} description={'will (pill)'}/>
+				</div>
 			</main>
 		</>
 	)
 }
 
 export default About;
-
