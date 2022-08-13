365b02b845537188e8fb1aaaf2d55bdc7e2e028b
David Chen <dchen07@protonmail.com>
2022-08-13 15:02:39 -0400

    Lint code with JetBrains WebStorm
    Adds `noinspection` comments for non-issues, and refactors files to be
    more efficiently defined.

--- lib/get_bell_schedule_by_date.ts
+++ lib/get_bell_schedule_by_date.ts
@@ -2,14 +2,11 @@ import Day from "../models/Day";
 import { ReceivedDay } from "../types/db_types";
 
 async function get_bell_schedule_by_date(date: string) {
-	const attempted_search: string =
-		(
+	return (
 			(await Day.findOne({
 				date: date,
 			})) as ReceivedDay
 		)?.bell_schedule_type || "Regular";
-
-	return attempted_search;
 }
 
 export default get_bell_schedule_by_date;
