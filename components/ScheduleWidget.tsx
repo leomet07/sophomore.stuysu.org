2f6ebb21ec93715b8e14698a551210f647824f8b
leomet07 <leomet07@gmail.com>
2022-08-14 08:12:35 +0300

    Get and print the current time in seconds, once per second

--- components/ScheduleWidget.tsx
+++ components/ScheduleWidget.tsx
@@ -1,11 +1,35 @@
 import styles from "../styles/ScheduleWidget.module.css";
 import { GradientPill, GradientShadow } from "./Gradient";
 import { ReceivedSchedule } from "../types/db_types";
+import { useEffect, useState } from "react";
+
+function timeToSeconds(time: string) {
+	let [shours, sminutes, sseconds, half] = time.split(/[\:\ ]/);
+	let hours = Number(shours);
+	let minutes = Number(sminutes);
+	let seconds = Number(sseconds);
+
+	if (half == "PM" && Number(hours) != 12) {
+		hours = Number(hours) + 12;
+	}
+	seconds = Number(seconds) + Number(minutes) * 60 + Number(hours) * 3600;
+	return seconds;
+}
 
 const ScheduleWidget = (props: {
 	current_schedule: ReceivedSchedule;
 	current_schedule_name: string;
 }) => {
+	useEffect(() => {
+		const id = setInterval(() => {
+			const ctime = new Date();
+			const locale_time_string = ctime.toLocaleTimeString("en-US");
+			const current_seconds = timeToSeconds(locale_time_string);
+
+			console.log("Current seconds", current_seconds);
+		}, 1000);
+		return () => clearInterval(id);
+	}, []);
 	return (
 		<div id={styles.container}>
 			<GradientPill
