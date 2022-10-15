b49f6dad0560002f787bf9a12781652e8292af2c
David Chen <dchen07@protonmail.com>
2022-10-14 21:37:26 -0400

    Add missing changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,26 +43,27 @@ const Navbar = () => {
 						<Image alt="" src={moonImg} />
 					</div>
 				</div>
+				<input id={styles.toggle} type="checkbox" />
 				<div className={styles.menu}>
-					<input id={styles.toggle} type="checkbox" />
 					<label id={styles.sandwich} htmlFor={styles.toggle}> <Image alt="" src={sandwichImg} /></label>
 					<label id={styles.x} htmlFor={styles.toggle}> <Image alt="" src={xImg} /></label>
-					<div className={styles.links_container}>
-						<div className={styles.link}>
-							<Link href="/opportunities">Opportunities</Link>
-						</div>
-						<div className={styles.link}>
-							<Link href="/contact">Contact</Link>
-						</div>
-						<div className={styles.link}>
-							<Link href="/policies">Policies</Link>
-						</div>
-						<div className={styles.link}>
-							<Link href="/about">About</Link>
-						</div>
+				</div>
+				<div className={styles.links_container}>
+					<div className={styles.link}>
+						<Link href="/opportunities">Opportunities</Link>
+					</div>
+					<div className={styles.link}>
+						<Link href="/contact">Contact</Link>
+					</div>
+					<div className={styles.link}>
+						<Link href="/policies">Policies</Link>
+					</div>
+					<div className={styles.link}>
+						<Link href="/about">About</Link>
 					</div>
 				</div>
-			</div>
+				</div>
+
 		</nav>
 	);
 };
