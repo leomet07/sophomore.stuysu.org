cdb83e95f277578c32d71be57c1e46586fd28344
leomet07 <leomet07@gmail.com>
2022-10-14 22:26:19 -0400

    Lint

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,34 +3,34 @@ import Link from "next/link";
 import logoImg from "../public/img/logo.png";
 import sunImg from "../public/img/sun.svg";
 import moonImg from "../public/img/moon.svg";
-import sandwichImg from "../public/img/sandwich.svg"
-import xImg from "../public/img/x.svg"
+import sandwichImg from "../public/img/sandwich.svg";
+import xImg from "../public/img/x.svg";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
 
-const NavLink = (props: {href: string, children: ReactNode}) => (
+const NavLink = (props: { href: string; children: ReactNode }) => (
 	<div className={styles.link}>
 		<Link href={props.href}>{props.children}</Link>
 	</div>
-)
+);
 
 const Navbar = () => {
-	const router = useRouter();  // used to remount/reset <nav> every navigation
+	const router = useRouter(); // used to remount/reset <nav> every navigation
 	return (
 		<nav id={styles.nav} key={router.asPath}>
 			<div>
-			<Link href="/" passHref>
-				<a>
-					<Image
-						src={logoImg}
-						id={styles.logo}
-						alt="Sophomore Caucus and the SU"
-						width="50"
-						height="50"
-					/>
-				</a>
-			</Link>
+				<Link href="/" passHref>
+					<a>
+						<Image
+							src={logoImg}
+							id={styles.logo}
+							alt="Sophomore Caucus and the SU"
+							width="50"
+							height="50"
+						/>
+					</a>
+				</Link>
 			</div>
 			<div id={styles.buttons}>
 				<div
@@ -56,17 +56,23 @@ const Navbar = () => {
 				</div>
 				<input id={styles.toggle} type="checkbox" />
 				<label htmlFor={styles.toggle} className={styles.menu}>
-					<div id={styles.sandwich}> <Image alt="" src={sandwichImg} /></div>
-					<div id={styles.x}> <Image alt="" src={xImg} /></div>
+					<div id={styles.sandwich}>
+						<Image alt="" src={sandwichImg} />
+					</div>
+					<div id={styles.x}>
+						<Image alt="" src={xImg} />
+					</div>
 				</label>
-				<label htmlFor={styles.toggle} className={styles.links_container}>
+				<label
+					htmlFor={styles.toggle}
+					className={styles.links_container}
+				>
 					<NavLink href={"/opportunities"}>Opportunities</NavLink>
 					<NavLink href="/contact">Contact</NavLink>
 					<NavLink href="/policies">Policies</NavLink>
 					<NavLink href="/about">About</NavLink>
 				</label>
-				</div>
-
+			</div>
 		</nav>
 	);
 };
