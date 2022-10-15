d733fb325d969a0f2e7378e14e2bb67a892a2817
David Chen <dchen07@protonmail.com>
2022-10-14 22:35:06 -0400

    Add nav drawer for mobile views, add Opportunities link to navbar (#19)
    * Add Opportunities link, non-functional sandwich
    
    * Begin crude functionality implementation
    
    * Implement animations, fix z-index
    
    * Add missing changes
    
    * Make drawer auto-close on navigation
    
    * Reset drawer on navigation (no residual state)
    
    * Lint
    
    Co-authored-by: leomet07 <leomet07@gmail.com>, TheEgghead27 @TheEgghead27

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,23 +3,36 @@ import Link from "next/link";
 import logoImg from "../public/img/logo.png";
 import sunImg from "../public/img/sun.svg";
 import moonImg from "../public/img/moon.svg";
+import sandwichImg from "../public/img/sandwich.svg";
+import xImg from "../public/img/x.svg";
 import Image from "next/image";
+import { ReactNode } from "react";
+import { useRouter } from "next/router";
+
+const NavLink = (props: { href: string; children: ReactNode }) => (
+	<div className={styles.link}>
+		<Link href={props.href}>{props.children}</Link>
+	</div>
+);
 
 const Navbar = () => {
+	const router = useRouter(); // used to remount/reset <nav> every navigation
 	return (
-		<nav id={styles.nav}>
-			<Link href="/" passHref>
-				<a>
-					<Image
-						src={logoImg}
-						id={styles.logo}
-						alt="Sophomore Caucus and the SU"
-						width="50"
-						height="50"
-					/>
-				</a>
-			</Link>
-			<div id={styles.links_container}>
+		<nav id={styles.nav} key={router.asPath}>
+			<div>
+				<Link href="/" passHref>
+					<a>
+						<Image
+							src={logoImg}
+							id={styles.logo}
+							alt="Sophomore Caucus and the SU"
+							width="50"
+							height="50"
+						/>
+					</a>
+				</Link>
+			</div>
+			<div id={styles.buttons}>
 				<div
 					className={styles.toggle}
 					onClick={() => {
@@ -41,15 +54,24 @@ const Navbar = () => {
 						<Image alt="" src={moonImg} />
 					</div>
 				</div>
-				<div className={styles.link}>
-					<Link href="/contact">Contact</Link>
-				</div>
-				<div className={styles.link}>
-					<Link href="/policies">Policies</Link>
-				</div>
-				<div className={styles.link}>
-					<Link href="/about">About</Link>
-				</div>
+				<input id={styles.toggle} type="checkbox" />
+				<label htmlFor={styles.toggle} className={styles.menu}>
+					<div id={styles.sandwich}>
+						<Image alt="" src={sandwichImg} />
+					</div>
+					<div id={styles.x}>
+						<Image alt="" src={xImg} />
+					</div>
+				</label>
+				<label
+					htmlFor={styles.toggle}
+					className={styles.links_container}
+				>
+					<NavLink href={"/opportunities"}>Opportunities</NavLink>
+					<NavLink href="/contact">Contact</NavLink>
+					<NavLink href="/policies">Policies</NavLink>
+					<NavLink href="/about">About</NavLink>
+				</label>
 			</div>
 		</nav>
 	);
